97fbcbe77f995f68784070743cb16608cf589b91
Susan Don <susankutu88@gmail.com>
2023-07-18 21:40:13 +0545

    checking

---
+++ src/assets/icons/index.js
@@ -0,0 +1,16 @@
+import {
+  FaHeart,
+  FaTwitter,
+  FaLinkedinIn,
+  FaFacebook,
+  FaSnapchat,
+  FaPinterest,
+} from "react-icons/fa";
+export {
+  FaHeart,
+  FaTwitter,
+  FaLinkedinIn,
+  FaFacebook,
+  FaSnapchat,
+  FaPinterest,
+};
