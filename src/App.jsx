db4499e28915ac2af9f295b5746970606fb9505a
Susan Don <susankutu88@gmail.com>
2023-07-20 14:12:45 +0545

    created slider and updated global css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import MusicSlider from "./components/Carousel";
 import Navbar from "./layouts/navbar/index";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <MusicSlider />
     </div>
   );
 }
